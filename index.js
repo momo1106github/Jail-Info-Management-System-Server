6259cfa4a1d32f6968df46be437d6dbff438266b
momo <nomofika@gmail.com>
2021-09-27 22:59:24 -0700

    init AplloServer

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import http from "http";
+import { makeExecutableSchema } from "graphql-tools-fork";
+import { importSchema } from "graphql-import";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import config from "./config";
 
 mongoose.connect(config.mongoURL, {
@@ -17,3 +22,35 @@ db.on("error", (error) => {
 db.once("open", () => {
   console.log("MongoDB connected!");
 });
+
+async function startApolloServer() {
+  const app = express();
+
+  const httpServer = http.createServer(app);
+
+  const schema = makeExecutableSchema({
+    typeDefs: importSchema("./schema.graphql"),
+    resolvers: {
+      Query,
+      Mutation,
+    },
+  });
+
+  const server = new ApolloServer({
+    schema: schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app }); // TODO: might need to add path and/or cors
+
+  httpServer.listen(config.port || 5000, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`🚀 Server listen on ${config.port || 5000}...`);
+  });
+}
+startApolloServer();
