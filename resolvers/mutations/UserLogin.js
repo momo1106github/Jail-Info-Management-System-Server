1bd3b5de70791e8249ec1cc5aa9b00999cb82c81
momo <nomofika@gmail.com>
2021-10-14 16:10:00 -0700

    change Id to ID

--- resolvers/mutations/UserLogin.js
+++ resolvers/mutations/UserLogin.js
@@ -3,17 +3,17 @@ import User from "../../models/user";
 
 const UserLogin = async (
   parent,
-  { deptId, password, role },
+  { deptID, password, role },
   { db, pubsub },
   info
 ) => {
-  const user = await User.findOne({ deptId });
+  const user = await User.findOne({ deptID });
   if (!user) throw new Error("USER_NOT_EXISTS");
 
   if (!(await bcrypt.compare(password, user.passwordHash)))
     throw new Error("INCORRECT_PASSWORD");
 
-  return deptId;
+  return deptID;
 };
 
 export default UserLogin;
--- resolvers/mutations/UserSignUp.js
+++ resolvers/mutations/UserSignUp.js
@@ -3,21 +3,21 @@ import User from "../../models/user";
 
 const UserSignUp = async (
   parent,
-  { deptId, password, role },
+  { deptID, password, role },
   { db, pubsub },
   info
 ) => {
-  const existedUser = await User.findOne({ deptId });
+  const existedUser = await User.findOne({ deptID });
   if (existedUser) throw new Error("ACCOUNT_ALREADY_EXISTS");
 
   const passwordHash = await bcrypt.hash(password, 10);
   await User.create({
-    deptId,
+    deptID,
     passwordHash,
     role,
   });
 
-  return deptId;
+  return deptID;
 };
 
 export default UserSignUp;
