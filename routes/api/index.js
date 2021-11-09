7d95770b08a8f7fce36681d71dc01d038f9bc3cd
momo <nomofika@gmail.com>
2021-11-08 17:52:08 -0800

    add booking router

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
+import { tsCallSignatureDeclaration } from "@babel/types";
 import { Router } from "express";
+import test from "../../models/Test";
 import userRouter from "./user";
+import bookingRouter from "./booking";
 
 const router = Router();
 
 router.use("/user", userRouter);
+router.use("/booking", bookingRouter);
 
 export default router;
