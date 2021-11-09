476010139772225538cdf2aa027e8eaaa5aadcc0
momo <nomofika@gmail.com>
2021-11-08 18:01:15 -0800

    add api for booking CRUD & getDailyLog()

---
+++ routes/api/booking.js
@@ -0,0 +1,64 @@
+import { Router } from "express";
+import BookingService from "../../services/BookingService";
+
+const router = Router();
+
+// /api/booking/
+router.get("/", (req, res) => {
+  console.log(req.body);
+
+  const { bookingNumber } = req.body;
+  BookingService.getBooking({ bookingNumber })
+    .then((booking) => {
+      console.log("Booking Information:\n", booking);
+      res.status(200).send(booking);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(401).send("BOOKING_INFORMATION_NOT_EXISTS");
+    });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { deptID, booking } = req.body;
+  BookingService.upsertBooking({ deptID, booking })
+    .then(() => {
+      console.log("Booking upserted:", booking);
+      res.status(200).send("BOOKING UPSERT SUCCESS");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).send(err);
+    });
+});
+
+router.delete("/", (req, res) => {
+  console.log(req.body);
+
+  const { bookingNumber } = req.body;
+  BookingService.deleteBooking({ bookingNumber })
+    .then(() => {
+      console.log("Booking deleted:", bookingNumber);
+      res.status(200).send("BOOKING DELETE SUCCESS");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).send(err);
+    });
+});
+
+// /api/booking/daily-log
+router.get("/daily-log", (req, res) => {
+  BookingService.getDailyLog()
+    .then((dailyLog) => {
+      console.log("Daily Booking Log:\n", dailyLog);
+      res.status(200).send(dailyLog);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+export default router;
