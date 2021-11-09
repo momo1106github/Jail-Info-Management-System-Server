85c527d72f4fb5ef27261cc4184587ea94e444ea
momo <nomofika@gmail.com>
2021-11-08 17:49:57 -0800

    fix typo

--- services/BookingService.js
+++ services/BookingService.js
@@ -10,7 +10,7 @@ class BookingService {
     return booking;
   }
 
-  static async UpsertBooking({ deptID, booking }) {
+  static async upsertBooking({ deptID, booking }) {
     const update = {
       name: booking.name,
       AKAs: booking.AKAs,
