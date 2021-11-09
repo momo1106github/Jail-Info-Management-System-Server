459f027e49dd24825df327b2d93e7ddcb3d06b3f
momo <nomofika@gmail.com>
2021-11-08 17:51:08 -0800

    change user role name from admin to supervisor

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   deptID: String,
   passwordHash: String,
-  role: { type: String, enums: ["inmate", "clerk", "admin"] }, // TODO
+  role: { type: String, enums: ["clerk", "supervisor"] }, // TODO
 });
 
 export default mongoose.model("User", userSchema);
